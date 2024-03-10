d018ac5a9a1135194c3be9e98da94ad35f0b867a
Martin Acosta <tinchoz49@gmail.com>
2024-03-10 12:03:01 -0300

    update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
+/* eslint perfectionist/sort-objects: "error" */
+
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { antfu, typescript } from '@antfu/eslint-config'
+
+import { antfu } from '@antfu/eslint-config'
 import { FlatCompat } from '@eslint/eslintrc'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -11,51 +14,80 @@ const compat = new FlatCompat({
   resolvePluginsRelativeTo: __dirname,
 })
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
 export default antfu(
   {
-    typescript: false,
+    astro: true,
     formatters: {
+      astro: true,
       css: true,
       html: true,
       markdown: true,
+      prettierOptions: {
+        jsxSingleQuote: true,
+      },
     },
     rules: {
-      'no-undef': ['error'],
-      'curly': ['error', 'multi-line'],
-      'no-console': 0,
       'antfu/if-newline': 0,
+      'curly': ['error', 'multi-line'],
+      'import/extensions': ['error', 'ignorePackages'],
+      'import/order': 0,
+      'no-console': 0,
+      'no-undef': ['error'],
+      'perfectionist/sort-exports': 'error',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          groups: [
+            'type',
+            'internal-type',
+            'builtin',
+            'external',
+            'internal',
+            ['parent-type', 'sibling-type', 'index-type'],
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+          ],
+          order: 'asc',
+          type: 'natural',
+        },
+      ],
+      'perfectionist/sort-named-exports': 'error',
+      'perfectionist/sort-named-imports': 'error',
+      'perfectionist/sort-objects': 'off',
+      'sort-imports': 0,
       'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'style/jsx-quotes': ['error', 'prefer-single'],
+      'style/quote-props': ['error', 'consistent-as-needed'],
+      'test/no-only-tests': 'error',
+      'yaml/quotes': ['error', {
+        avoidEscape: true,
+        prefer: 'double',
+      }],
+    },
+    stylistic: {
+      quotes: 'single',
+    },
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
     },
   },
-  typescript({
-    files: ['**/*.ts', '**/*.tsx', '**/*.astro'],
-    componentExts: ['.astro'],
-  }),
   ...compat.config({
     overrides: [{
-      files: ['**/*.astro'],
+      env: {
+        'astro/astro': true,
+        'es2020': true,
+        // Enables global variables available in Astro components.
+        'node': true,
+      },
       extends: [
-        'plugin:astro/recommended',
         'plugin:astro/jsx-a11y-recommended',
       ],
+      files: ['**/*.astro'],
       globals: {
         astroHTML: true,
       },
-      env: {
-        // Enables global variables available in Astro components.
-        'node': true,
-        'astro/astro': true,
-        'es2020': true,
-      },
       parser: 'astro-eslint-parser',
-      rules: {
-        // this is necessary to force a correct indentation in astro
-        'style/indent': ['error', 2],
-        'style/jsx-indent': 'off',
-        'style/jsx-one-expression-per-line': 'off',
-        'unused-imports/no-unused-vars': 'off',
-      },
     }],
   }),
   {
