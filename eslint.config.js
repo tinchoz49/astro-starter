1ff985cdb27f16b64033ce533c58338eb097ef60
Martin Acosta <tinchoz49@gmail.com>
2024-06-01 10:37:59 -0300

    add: tailwind eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
 /* eslint perfectionist/sort-objects: "error" */
 
 import { standard } from 'eslint-config-standard-ext'
+import tailwind from 'eslint-plugin-tailwindcss'
 
 export default standard(
   {
     astro: {
       a11y: true,
     },
-    formatters: {
-      astro: true,
-      css: true,
-      html: true,
-      markdown: true,
-    },
+    formatters: false,
     typescript: {
       tsconfigPath: 'tsconfig.json',
     },
@@ -21,5 +17,11 @@ export default standard(
     ignores: [
       'public/',
     ],
+  },
+  ...tailwind.configs['flat/recommended'],
+  {
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+    },
   }
 )
