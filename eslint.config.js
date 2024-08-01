d6dd844dac6492095d64abb140d32c33ee53074c
Martin Acosta <tinchoz49@gmail.com>
2024-08-01 00:43:57 -0300

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
 /* eslint perfectionist/sort-objects: "error" */
 
 import { standard } from 'eslint-config-standard-ext'
-import astroPlugin from 'eslint-plugin-astro'
 import tailwind from 'eslint-plugin-tailwindcss'
-
-const a11yConfig = astroPlugin.configs['jsx-a11y-recommended'].find(c => !!c?.plugins?.['jsx-a11y'])
 
 const config = standard(
   {
-    astro: true,
-    formatters: true,
+    astro: {
+      config: 'jsx-a11y-recommended',
+    },
+    formatters: {
+      css: true,
+      html: true,
+      markdown: true,
+    },
     markdown: true,
     typescript: true,
   },
-  a11yConfig,
   ...tailwind.configs['flat/recommended'],
   {
     rules: {
